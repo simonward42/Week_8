1ff3654d6764410e0b7874353b76e0c85e225ab1
Simon Ward <simon.ward42@gmail>
2012-11-26 15:55:31 +0000

    modified version: now asks how many numbers the user wants to enter at startup

--- MeanAverage.java
+++ MeanAverage.java
@@ -5,26 +5,34 @@ public class MeanAverage {
 	}
 	
 	public void run() {
-		int[] input = new int[10];
-		for(int i=0; i<10; i++) {
-			System.out.println("Enter an integer:\t");
+		System.out.println("How many numbers do you want to enter?");
+		int divisor = 0;
+		while(divisor == 0) {
+			divisor = getUserInput();
+			if(divisor == 0) {
+				System.out.println("but it can't be equal to zero!");
+			}
+		}
+		int[] input = new int[divisor];
+		for(int i=0; i<divisor; i++) {
+			System.out.print("Enter an integer:\t");
 			input[i] = getUserInput();
 		}
-		double mean;
-		for(int i=0; i<10; i++) {
+		double mean = 0;
+		for(int i=0; i<divisor; i++) {
 			mean += input[i];
 		}
-		mean = mean / 10;
+		mean = mean / divisor;
 		System.out.println("The mean average of the numbers you entered is " + mean);
 	}
 	
 	public int getUserInput() {
-		int value;
+		int value = 0;
 		boolean gotValidInput = false;
 		while(!gotValidInput) {
 			try {
-			value = Integer.parseInt(System.console.readLine());
-			gotValidInput = true;
+				value = Integer.parseInt(System.console().readLine());
+				gotValidInput = true;
 			} catch (NumberFormatException ex) {
 				System.out.println("That's not a number! Please try again.");
 			}
